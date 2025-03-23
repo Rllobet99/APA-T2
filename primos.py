f05d4f0798455ddaf438dd9a03a313f797e1cd0f
Ramon Llobet Duch <ramon.llobet.duch@estudiantat.upc.edu>
2025-03-23 16:58:31 +0100

    funcions descompon i mcm

--- primos.py
+++ primos.py
@@ -61,9 +61,68 @@ def primos(numero):
 
     return tuple(lista)  # Se convierte la lista en una tupla
 
+    
+
+def descompon(numero): 
+    """
+    Devuelve una tupla con la descomposición en factores primos del número dado.
+
+    >>> descompon(10)
+    (2, 5)
+
+    >>> descompon(30)
+    (2, 3, 5)
+
+    >>> descompon(13)  # Es primo
+    (13,)
+
+    """
+    arg_primos = []
+    divisor = 2  # menor número primo
+
+    while numero > 1: 
+        while numero % divisor == 0: 
+            arg_primos.append(divisor)
+            numero //= divisor  
+        divisor += 1  
+
+    return tuple(arg_primos)
+
+
+
+def mcm(numero1,numero2):
+    """
+    Devuelve el mínimo común múltiplo (MCM) de dos números enteros.
+    El MCM se calcula a partir de los factores primos de ambos números.
+    
+    >>> mcm(50, 30)
+    150
+    
+    >>> mcm(15, 20)
+    60
+    
+    """
+     factors1 = descompon(numero1)
+     factors2 = descompon(numero2)
+     comuns =   set(factors1 + factors2)
+     resultat = 1
+     for num in comuns:
+          exp1 = factors1.count(num)
+          exp2 = factors2.count(num)
+          expo_max = max(exp1,exp2)
+          resultat = resultat*num**expo_max
+    
+     return resultat
+
+
+
+
+
+
+     
+
         
-#PENDIENTE:
-# descompon(numero) Devuelve una tupla con la descomposición en factores primos de su argumento.         
+
 
 
 
