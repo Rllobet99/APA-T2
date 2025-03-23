e4f6fc98e9ef28aea2deb1d5bc885e0245ec70a4
Ramon Llobet Duch <ramon.llobet.duch@estudiantat.upc.edu>
2025-03-23 19:10:52 +0100

    actualizació 23/03/2025 mcm,mcd,mcm*,mcd*

--- primos.py
+++ primos.py
@@ -4,14 +4,7 @@
 
 Ramon Llobet Duch
 
->>> esPrimo(5)
-True
-
->>> esPrimo(10)
-False
-
->>> esPrimo(74211)
-False
+Determinación de la primalidad y mínimo común múltiplo y máximo común divisor
 
 
 """
@@ -51,6 +44,9 @@ def primos(numero):
     >>> primos(20)
     (2, 3, 5, 7, 11, 13, 17, 19)
 
+    >>> primos(50)
+    (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
+
     >>> primos(2)
     ()
     """
@@ -66,7 +62,9 @@ def primos(numero):
 def descompon(numero): 
     """
     Devuelve una tupla con la descomposición en factores primos del número dado.
-
+    >>> descompon(36 * 175 * 143)
+    (2, 2, 3, 3, 5, 5, 7, 11, 13)
+    
     >>> descompon(10)
     (2, 5)
 
@@ -91,6 +89,7 @@ def descompon(numero):
 
 
 def mcm(numero1,numero2):
+
     """
     Devuelve el mínimo común múltiplo (MCM) de dos números enteros.
     El MCM se calcula a partir de los factores primos de ambos números.
@@ -100,32 +99,77 @@ def mcm(numero1,numero2):
     
     >>> mcm(15, 20)
     60
-    
+
     """
-     factors1 = descompon(numero1)
-     factors2 = descompon(numero2)
-     comuns =   set(factors1 + factors2)
-     resultat = 1
-     for num in comuns:
-          exp1 = factors1.count(num)
-          exp2 = factors2.count(num)
-          expo_max = max(exp1,exp2)
-          resultat = resultat*num**expo_max
+    factors1 = descompon(numero1)
+    factors2 = descompon(numero2)
+    comuns =   set(factors1) | set(factors2)
+    resultat = 1
+    for num in comuns:
+        exp1 = factors1.count(num)
+        exp2 = factors2.count(num)
+        expo_max = max(exp1,exp2)
+        resultat = resultat*num**expo_max
     
-     return resultat
+    return resultat
 
 
 
+def mcd(numero1,numero2):
+
+    """
+    Devuelve el máximo común divisor (MCD) de dos números.
+
+    El MCD se calcula descomponiendo los números en sus factores primos,
+    identificando los factores comunes y tomando la mínima potencia de cada uno.
+
+    >>> mcd(924, 780)
+    12
+
+    >>> mcd(36, 48)
+    12
+
+    """
+     
+    factors1 = descompon(numero1)
+    factors2 = descompon(numero2)
+    comuns = set(factors1) &set(factors2)
+    resultat = 1
+    for num in comuns:
+        exp1 = factors1.count(num)
+        exp2 = factors2.count(num)
+        expo_min = min(exp1,exp2)
+        resultat = resultat*num**expo_min
+    return resultat
 
 
+def mcmN(*numeros):
+    """
+    Devuelve el mínimo común múltiplo de sus argumentos.
+    >>> mcmN(42, 60, 70, 63)
+    1260
+    """
+    acumulado = numeros[0]
+
+    for numero in numeros[1:]:
+        acumulado = mcm(acumulado,numero)
+
+    return acumulado
+
+def mcdN(*numeros): 
+    """
+    Devuelve el máximo común divisor de sus argumentos.
+    >>> mcdN(840, 630, 1050, 1470)
+    210
+    """
+    acumulado = numeros[0]
+
+    for numero in numeros[1:]:
+        acumulado = mcd(acumulado,numero)
+
+    return acumulado
 
      
-
-        
-
-
-
-
 
 
 
